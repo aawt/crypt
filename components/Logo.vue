30251921701871078a83b5802374fd6a807597f5
aaron taylor <aarontaylor@gmail.com>
2019-05-24 21:56:46 -0700

    setup project, add some style and header image

---
+++ components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+
+ <div class="container">
+  <img src="/cryptkeeper.png" class="Logo" />
+      <p class="Title">
+      _ Crypt Keeper _
+      </p>
+
+  </div>
+
+</template>
+
+<style>
+.container {
+  margin: 0 0 0 0;
+  text-align: center;
+}
+img .Logo {
+  position: absolute;
+  z-index: -1;
+  left: 0px;
+  top: 0px;
+}
+.Title {
+  margin-top: -90px;
+  font-size: 22px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+ <logo />
+
+</div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+</style>
